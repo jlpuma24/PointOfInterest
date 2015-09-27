6aa75dcd79df7baa1109486f4b9933d93b8fe224
Gabriel Rodriguez <gabriel.rodriguez3103@gmail.com>
2015-09-27 01:03:31 -0430

    Search

--- app/src/main/java/com/wuelto/prueba/com/pointofinterest/activitys/MainActivity.java
+++ app/src/main/java/com/wuelto/prueba/com/pointofinterest/activitys/MainActivity.java
@@ -3,8 +3,11 @@ package com.wuelto.prueba.com.pointofinterest.activitys;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.wuelto.prueba.com.pointofinterest.R;
@@ -23,18 +26,49 @@ public class MainActivity extends Activity {
     private ListView mListView;
     private ArrayList<Places> mPlacesArrayList;
     private DataBaseHelper mDataBaseHelper;
+    private Intent intent;
+    private String searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mDataBaseHelper = new DataBaseHelper(this);
         setContentView(R.layout.activity_main);
-        if (ConnectionManager.isConnected()) {
+        intent = getIntent();
+        try {
+            if (intent.getStringExtra("search") != null) {
+                searchText = intent.getStringExtra("search");
+            }
+            else {
+                searchText = "";
+            }
+
+        } catch (NullPointerException e) {
+            searchText = "";
+            Log.v("SearchText", e.getMessage());
+        }
+
+        ImageButton imageButton = (ImageButton)findViewById(R.id.button_search);
+        imageButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchText = ((EditText)findViewById(R.id.search_text)).getText().toString();
+                if (searchText.length() !=0) {
+                    Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                    intent.putExtra("search",searchText);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+        if (ConnectionManager.isConnected() && mDataBaseHelper.select()!= null) {
             mJsonFileAsyncTask = new FetchJsonAsyncTask(this);
             mJsonFileAsyncTask.execute();
         } else {
-            mDataBaseHelper = new DataBaseHelper(this);
             notifyFinishingAsyncTask(false);
         }
+
     }
 
     public void notifyFinishingAsyncTask (boolean isConnected) {
@@ -44,14 +78,29 @@ public class MainActivity extends Activity {
         } else {
             mPlacesArrayList = mDataBaseHelper.select();
         }
-
+        ArrayList<Places> searchArrayList = new ArrayList<Places>();
         mListView = (ListView)findViewById(R.id.listview_places);
-        mListView.setAdapter(new CustomAdapter(this,mPlacesArrayList,mListView));
+
+        if (searchText != "") {
+            for (Places place : mPlacesArrayList) {
+                if (place.getName().contains(searchText)) {
+                    searchArrayList.add(place);
+                }
+            }
+            mListView.setAdapter(new CustomAdapter(this,searchArrayList,mListView));
+        } else {
+            mListView.setAdapter(new CustomAdapter(this,mPlacesArrayList,mListView));
+        }
+
+
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-                intent.putExtra("Description",mPlacesArrayList.get(position).getDescription());
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                intent.putExtra("description",mPlacesArrayList.get(position).getDescription());
+                intent.putExtra("latitude", mPlacesArrayList.get(position).getLat());
+                intent.putExtra("longitude", mPlacesArrayList.get(position).getLon());
+                intent.putExtra("title", mPlacesArrayList.get(position).getName());
                 startActivity(intent);
             }
         });
